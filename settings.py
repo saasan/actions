26bc42297d3d51c1f46275790160d6531ed824e7
saasan <saasan.s2works@hotmail.com>
2023-07-09 20:31:29 +0900

    test

--- settings.py
+++ settings.py
@@ -4,6 +4,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
+    # ツイート済み日時が保存されていない場合にツイートする最大数
+    untweeted_max_num: int = 3
+
     # ツイート済み日時を書き込むファイル名
     tweeted_file: str = 'tweeted'
     # ログ出力の設定ファイル
